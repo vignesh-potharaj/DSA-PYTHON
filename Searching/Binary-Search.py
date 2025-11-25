e2426e04108a39b9ab7e38ad8302e86fce641de6
vignes-potharaj <freelancer.nenu@gmail.com>
2025-11-25 11:02:20 +0530

    implement binary search algorithm

---
+++ Searching/Binary-Search.py
@@ -0,0 +1,18 @@
+numbers = [10, 20, 30, 40, 50, 60]
+
+def binarySearch(arr, target):
+    low = 0
+    high = len(arr) - 1
+    found = False
+    while low <= high:
+        mid = (low + high) / 2
+        if arr[mid] == target:
+            found = True
+            break
+        elif numbers[mid] < target:
+            low = mid +1
+        else:
+            high = mid -1
+    return found    
+
+print(binarySearch(numbers, 30))
